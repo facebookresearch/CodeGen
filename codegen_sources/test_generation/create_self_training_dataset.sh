234a461a31ea8decf3b2d31b5c14ce35ac5a47b9
Baptiste Rozière <broz@fb.com>
2021-10-21 17:09:08 +0200

    adding missing files

---
+++ codegen_sources/test_generation/create_self_training_dataset.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Script to create the dataset for self-training
+
+set -e
+
+JAVA_FUNC_DATASET=$1 # "/private/home/broz/datasets/CodeGen/data/CodeGen_datasets/cpp_java_python_function_level_XLM_test/"
+PATH_TO_TRANSLATION_MODELS=$2 # "/checkpoint/broz/Transcoder_saved_models/"
+ST_OUTPUT_ROOT_DIR=$3 # "/private/home/broz/data/automatic_tests/test_data/"
+RUN_SCRIPT_LOCALLY=True
+RERUN_ALL_CHUNKS=False
+REPO_ROOT="."
+echo "Repository root: $REPO_ROOT"
+
+SELECTED_FUNCS="${ST_OUTPUT_ROOT_DIR}selected_functions/deduped/"
+TESTS_PATHS="${ST_OUTPUT_ROOT_DIR}tests/"
+TRANSLATIONS_AND_RESULTS_PATH="${ST_OUTPUT_ROOT_DIR}results/"
+TESTS_RESULTS="${TRANSLATIONS_AND_RESULTS_PATH}test_results/"
+BPE_CODES_PATH="$REPO_ROOT/data/bpe/cpp-java-python/codes"
+BPE_VOCAB_PATH="$REPO_ROOT/data/bpe/cpp-java-python/vocab"
+PYTHON_BEST_MODEL="${PATH_TO_TRANSLATION_MODELS}translator_transcoder_size_from_DOBF.pth"
+CPP_BEST_MODEL="${PATH_TO_TRANSLATION_MODELS}TransCoder_model_1.pth"
+SELECTED_TESTS_PATH="${TESTS_PATHS}selected_tests.csv"
+OFFLINE_DATASET_PATH="${ST_OUTPUT_ROOT_DIR}offline_dataset/"
+ONLINE_OUTPUT_PATH="${ST_OUTPUT_ROOT_DIR}online_ST_files/"
+
+
+# Select functions to create tests on
+echo "python codegen_sources/test_generation/select_java_inputs.py --local $RUN_SCRIPT_LOCALLY --input_path $JAVA_FUNC_DATASET --output_path $SELECTED_FUNCS --rerun $RERUN_ALL_CHUNKS"
+python codegen_sources/test_generation/select_java_inputs.py --local $RUN_SCRIPT_LOCALLY --input_path $JAVA_FUNC_DATASET --output_path $SELECTED_FUNCS --rerun $RERUN_ALL_CHUNKS
+
+# Create the tests
+echo "python codegen_sources/test_generation/create_tests.py --local $RUN_SCRIPT_LOCALLY --input_path $SELECTED_FUNCS --output_path $TESTS_PATHS --rerun $RERUN_ALL_CHUNKS"
+python codegen_sources/test_generation/create_tests.py --local $RUN_SCRIPT_LOCALLY --input_path $SELECTED_FUNCS --output_path $TESTS_PATHS --rerun $RERUN_ALL_CHUNKS
+
+# Compute translations and tests results for Python
+python codegen_sources/test_generation/compute_transcoder_translations.py \
+  --functions_path $SELECTED_FUNCS \
+  --csv_path $SELECTED_TESTS_PATH \
+  --output_folder $TRANSLATIONS_AND_RESULTS_PATH \
+  --bpe_path $BPE_CODES_PATH \
+  --target_language cpp \
+  --model_path $CPP_BEST_MODEL \
+  --local $RUN_SCRIPT_LOCALLY \
+  --rerun $RERUN_ALL_CHUNKS
+# Compute translations and tests results for C++
+python codegen_sources/test_generation/compute_transcoder_translations.py \
+  --functions_path $SELECTED_FUNCS \
+  --csv_path $SELECTED_TESTS_PATH \
+  --output_folder $TRANSLATIONS_AND_RESULTS_PATH \
+  --bpe_path $BPE_CODES_PATH \
+  --target_language python \
+  --model_path $PYTHON_BEST_MODEL \
+  --local $RUN_SCRIPT_LOCALLY \
+  --rerun $RERUN_ALL_CHUNKS
+
+# Select tests and create dataset for offline training
+python codegen_sources/test_generation/select_successful_tests.py \
+  --input_df $TESTS_RESULTS \
+  --output_folder $OFFLINE_DATASET_PATH \
+  --bpe_path $BPE_CODES_PATH \
+  --bpe_vocab $BPE_VOCAB_PATH
+
+# Create files for online dataset
+echo "python codegen_sources/test_generation/create_data_for_online_st.py --dataset_path $OFFLINE_DATASET_PATH --input_dfs_path $TESTS_RESULTS --output_path $ONLINE_OUTPUT_PATH --vocab_path $BPE_VOCAB_PATH"
+python codegen_sources/test_generation/create_data_for_online_st.py \
+  --dataset_path $OFFLINE_DATASET_PATH \
+  --input_dfs_path $TESTS_RESULTS \
+  --output_path $ONLINE_OUTPUT_PATH \
+  --vocab_path $BPE_VOCAB_PATH
+
+# add transcoder test set
+cd $ONLINE_OUTPUT_PATH
+wget https://dl.fbaipublicfiles.com/transcoder/test_set/transcoder_test_set.zip
+unzip transcoder_test_set.zip
+ln -s test_dataset/*.pth .
+
